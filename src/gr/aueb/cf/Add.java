b9303b5c497c7539955c797b5cba6d5a5c06dbe6
yiannisdseq <yiannisdseq@gmail.com>
2026-05-20 01:30:13 +0300

    feat: add a new addition method

---
+++ src/gr/aueb/cf/Add.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf;
+
+public class Add {
+
+    public static void main(String[] args) {
+        System.out.println(add(3, 5));
+
+
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
