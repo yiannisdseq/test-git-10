98cb658dad7504f110e8cbbe93cfc5768d0378db
yiannisdseq <yiannisdseq@gmail.com>
2026-05-20 01:46:11 +0300

    fix: change World to CF in println

--- src/gr/aueb/cf/Main.java
+++ src/gr/aueb/cf/Main.java
@@ -3,7 +3,7 @@ package gr.aueb.cf;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello CF!!");
 
 
     }
